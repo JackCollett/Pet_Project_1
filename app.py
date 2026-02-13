7fe6f3bc42857f2a32f04d7ecee45061990fa29e
Jack Collett <jack.collett@live.co.uk>
2026-02-13 17:13:21 +0000

    secret key and empty user names

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
-import os, requests, psycopg2, json
+import os, requests, psycopg2, json, secrets
 from lib.media_repository import MediaRepository
 from lib.media import Media
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMENANT"] = False
+app.config["SECRET_KEY"] = secrets.token_hex(32)
+app.config["SESSION_PERMANANT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -102,8 +103,12 @@ if __name__ == '__main__':
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        session["name"] = request.form.get("name")
-        return redirect("/")
+        name = request.form.get("name")
+
+        if name: # prevents black usernames
+            session["name"] = name
+            return redirect("/")
+        
     return render_template("login.html")
 
 @app.route("/logout")
